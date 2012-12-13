440555b553efc141438a9e6f52d4ef93e93cd0d9
Robert Elwell <robert@wikia-inc.com>
2012-12-13 10:27:21 -0800

    Refactored SimpleSummarizer to return a list instead of joined sentences

--- summarize.py
+++ summarize.py
@@ -1,16 +1,16 @@
-# Simple Summarizer 
-# Copyright (C) 2010-2012 Tristan Havelick 
+# Simple Summarizer
+# Copyright (C) 2010-2012 Tristan Havelick
 # Author: Tristan Havelick <tristan@havelick.com>
 # URL: <https://github.com/thavelick/summarize/>
 # For license information, see LICENSE.TXT
 
 """
-A summarizer based on the algorithm found in Classifier4J by Nick Lothan.      
-In order to summarize a document this algorithm first determines the 
+A summarizer based on the algorithm found in Classifier4J by Nick Lothan.
+In order to summarize a document this algorithm first determines the
 frequencies of the words in the document.  It then splits the document
 into a series of sentences.  Then it creates a summary by including the
 first sentence that includes each of the most frequent words.  Finally
-summary's sentences are reordered to reflect that of those in the original 
+summary's sentences are reordered to reflect that of those in the original
 document.
 """
 
@@ -18,7 +18,7 @@ document.
 ##  Simple Summarizer
 ##//////////////////////////////////////////////////////
 
-from nltk.probability import FreqDist 
+from nltk.probability import FreqDist
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 import nltk.data
@@ -30,29 +30,29 @@ class SimpleSummarizer:
 			input.find(s1) - input.find(s2) )
 		return output_sentences
 	
-	def summarize(self, input, num_sentences ):
+	def get_summarized(self, input, num_sentences ):
 		# TODO: allow the caller to specify the tokenizer they want
 		# TODO: allow the user to specify the sentence tokenizer they want
 		
 		tokenizer = RegexpTokenizer('\w+')
 		
 		# get the frequency of each word in the input
-		base_words = [word.lower() 
+		base_words = [word.lower()
 			for word in tokenizer.tokenize(input)]
 		words = [word for word in base_words if word not in stopwords.words()]
 		word_frequencies = FreqDist(words)
 		
 		# now create a set of the most frequent words
-		most_frequent_words = [pair[0] for pair in 
+		most_frequent_words = [pair[0] for pair in
 			word_frequencies.items()[:100]]
 		
-		# break the input up into sentences.  working_sentences is used 
+		# break the input up into sentences.  working_sentences is used
 		# for the analysis, but actual_sentences is used in the results
 		# so capitalization will be correct.
 		
 		sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 		actual_sentences = sent_detector.tokenize(input)
-		working_sentences = [sentence.lower() 
+		working_sentences = [sentence.lower()
 			for sentence in actual_sentences]
 
 		# iterate over the most frequent words, and add the first sentence
@@ -61,7 +61,7 @@ class SimpleSummarizer:
 
 		for word in most_frequent_words:
 			for i in range(0, len(working_sentences)):
-				if (word in working_sentences[i] 
+				if (word in working_sentences[i]
 				  and actual_sentences[i] not in output_sentences):
 					output_sentences.append(actual_sentences[i])
 					break
@@ -69,7 +69,7 @@ class SimpleSummarizer:
 			if len(output_sentences) >= num_sentences: break
 			
 		# sort the output sentences back to their original order
-		output_sentences = self.reorder_sentences(output_sentences, input)
-
-		# concatinate the sentences into a single string
-		return "  ".join(output_sentences)
+		return self.reorder_sentences(output_sentences, input)
+	
+	def summarize(self, input, num_sentences):
+		return " ".join(self.get_summarized(input, num_sentences))
